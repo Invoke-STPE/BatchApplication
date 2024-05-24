b3348191f001e93842c7b1a5525a5d2ee77a51f6
Web-Basker <Web-Basker@outlook.com>
2024-05-24 13:01:09 +0200

    Setup remaining test structure.

---
+++ Application/JobStarter.cs
@@ -0,0 +1,42 @@
+﻿using Domain;
+using Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Application
+{
+    public static class JobStarter
+    {
+        private static readonly Dictionary<string, Type> _jobs = [];
+
+        static JobStarter()
+        {
+            Type[] batchJobsTypes = BatchJobAssemblyManager.GetLoadedBatchJobTypes();
+
+            foreach (var type in batchJobsTypes)
+            {
+                if (Activator.CreateInstance(type) is IBatchJob batchJob)
+                {
+                    _jobs.Add(batchJob.BatchId, type);
+                }
+            }
+        }
+
+        public static void StartJob(string jobId, IHost host)
+        {
+            var jobExists = _jobs.TryGetValue(jobId, out Type? jobType);
+
+            if (jobExists == false) { throw new ArgumentException($"Invalid job id: {jobId}"); }
+            
+            var service = host.Services.GetRequiredService(jobType!) as IBatchJob;
+            if (service is IBatchJob job)
+            {
+                job.Execute();
+            }
+            else
+            {
+                throw new InvalidOperationException($"Service of type {jobType} does not implement IBatchJob");
+            }
+        }
+    }
+}
